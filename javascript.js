7c7f91d548a3338ea4e66e27c57667eab78cc8c8
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-04-08 18:26:15 +0200

    Set custom validation message.
    Tweak the passwordConfirm() by using the
    setCustomValidity() method
    of the Constraint Validation API to set
    custom validity messages when passwords
    do not match. Also leave the
    custom validity message blank if
    the passwords match.
    (If I have a custom message, the fields
    are invalid but if I leave it blank "",
    it becomes valid)
    
    This makes the password fields invalid if
    the both fields do not match.
    
    Also change the class of the
    .pass-match-info span to green or red if passwords
    match or they don't, respectively.
    
    On the CSS, create the .green and .red class
    declaration and give them appropriate styles.

--- javascript.js
+++ javascript.js
@@ -4,14 +4,20 @@
 function passwordConfirm() {
     const password1 = document.querySelector("#password-1");
     const password2 = document.querySelector("#confirm-password");
-    const passwordMatchInfo = document.querySelector(".pass-match-info");
+    const passwordMatchInfo = document.querySelector("#pass-match-info");
 
 
     if ((password1.value === "") || (password2.value === "" )) {
         passwordMatchInfo.text = "";
     } else if (password1.value !== password2.value) {
+        password1.setCustomValidity("Please check your password.");
+        password2.setCustomValidity("Please check your password.");
+        passwordMatchInfo.classList = "red";
         passwordMatchInfo.textContent = "*Passwords don't match!";
     } else {
+        password1.setCustomValidity("");
+        password2.setCustomValidity("");
+        passwordMatchInfo.classList = "green";
         passwordMatchInfo.textContent = "Hmmm...looks nice."
     } 
 }
